88c423916c87bbbdbf34661cc6e246f0498a39fc
jonathsf <webmaximun@gmail.com>
2024-04-01 20:00:34 -0300

    adicionando os comentários no código

--- client.py
+++ client.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify, render_template, redirect
-from model.restaurant import db, Restaurant, Address
+#importa de bibliotecas externas
+from flask import Flask, request, render_template, redirect
+#importa de classes e métodos do model
+from model.restaurant import db
 import requests
 
-
+#
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/mba_arq_serv_restfull_python'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,15 +13,17 @@ PORT = 5000
 
 db.init_app(app)
 
+#endpoint inicial
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#endpoint de cadastro
 @app.route('/inserir' , methods=['GET', 'POST'])
 def inserir():  
    msg = ""
    if request.method == 'POST':
-        
+        #pega os dados do corpo da requisição
         nome = request.form['name']
         endereco = {
             'streetAddress': request.form['streetAddress'],
@@ -31,7 +35,7 @@ def inserir():
         menu = request.form.get('menu', '')
         telefone = request.form.get('telephone', '')
         faixa_preco = request.form.get('priceRange', '')
-        
+        #cria um novo restaurante
         novo_restaurante = {
             'name': nome,
             'address': endereco,
@@ -40,23 +44,27 @@ def inserir():
             'telephone': telefone,
             'priceRange': faixa_preco
         }
+
+        #envia o novo restaurante para o endpoint do server
         response = requests.post("http://localhost:4999/restaurant", json=novo_restaurante)
+
         if response.status_code == 201:
             msg = "Restaurante inserido com sucesso"        
    
    return render_template('insert_restaurant.html', msg = msg)
 
+#endpoint de edição
 @app.route('/editar/', defaults={'restaurant_id': None}, methods=['GET','PUT','POST'])
 @app.route('/editar/<restaurant_id>', methods=['GET','PUT', 'POST'])
 def atualizar(restaurant_id):
    
+    #acessa o endpoint do server passando o ID para localizar o restaurante
     response = requests.get("http://localhost:4999/restaurant/{}".format(restaurant_id))
     restaurant = response.json()   
-   
-    msg = ""
-    # print("-----------------------"+ request.json)
+    
     if request.method == 'POST':    
-
+        
+        #pega os dados do corpo da requisição
         nome = request.form['name']
         endereco = {
             'streetAddress': request.form['streetAddress'],
@@ -69,6 +77,7 @@ def atualizar(restaurant_id):
         telefone = request.form.get('telephone', '')
         faixa_preco = request.form.get('priceRange', '')
         
+        #atualiza os dados do restaurante
         atualizar_restaurante = {
             'name': nome,
             'address': endereco,
@@ -83,39 +92,26 @@ def atualizar(restaurant_id):
         if response.status_code == 200:            
             return redirect('/listar')
     
-    return render_template('update_restaurant.html', msg = msg, restaurant = restaurant)
+    return render_template('update_restaurant.html', restaurant = restaurant)
 
+#endpoint de listagem
 @app.route('/listar')
 def listar():   
+    #acessa o endpoint do server para listar todos os restaurantes
     response = requests.get("http://localhost:4999/restaurants")    
     restaurants = response.json()
     return render_template('list_restaurants.html', restaurants=restaurants)
 
-# @app.route('/buscar/')
-# def buscar_restaurante():
-#     restaurant_id = request.args.get('restaurant')
-    
-#     response = requests.get("http://localhost:4999/restaurants")    
-#     restaurants = response.json()
-       
-
-#     if restaurant_id is not None and restaurant_id != "":        
-#         response = requests.get("http://localhost:4999/restaurant/{}".format(restaurant_id))
-#         restaurant = response.json()
-#         restaurant = restaurant
-#     else:
-#         restaurant=None
-
-#     return render_template('find_restaurant.html',restaurants = restaurants, restaurant=restaurant, args = restaurant_id)
-
+#endpoint de busca
 @app.route('/buscar/', methods=['GET'])
 def buscar_restaurante():
-
+    #pega os dados do corpo da requisição
     addressLocality = request.args.get('addressLocality')
     addressRegion = request.args.get('addressRegion')
     addressCountry = request.args.get('addressCountry')
     streetAddress = request.args.get('streetAddress')
-
+    
+    #filtros
     parametros = {
             'addressLocality': addressLocality,
             'addressRegion': addressRegion,
@@ -123,14 +119,17 @@ def buscar_restaurante():
             'streetAddress': streetAddress
         }    
     
+    #acessa o endpoint do server para buscar os restaurantes
     response = requests.get('http://localhost:4999/restaurants/address', params=parametros)    
 
     return render_template('find_restaurants_by_fields.html',data = response.json())
 
 
+#endpoint de exclusão
 @app.route('/excluir/', defaults={'restaurant_id': None})
 @app.route('/excluir/<restaurant_id>')
 def excluir(restaurant_id):
+    #acessa o endpoint do server passando o ID para excluir o restaurante
     response = requests.delete("http://localhost:4999/restaurant/{}".format(restaurant_id))
 
     if response == 200:
--- serve.py
+++ serve.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify, render_template
+#importa de bibliotecas externas
+from flask import Flask, request, jsonify
+#importa de classes e métodos do model
 from model.restaurant import db, Restaurant, Address
-import requests
 
-
+#conexão com o banco de dados local
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/mba_arq_serv_restfull_python'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,14 +12,17 @@ PORT = 4999
 
 db.init_app(app)
 
+#Endpoint de cadastro do restaurante
 @app.route('/restaurant', methods=['POST'])
 def add_restaurant():
 
+    #pega os dados do corpo da requisição
     data = request.json
-
+    #verifica se os campos nome e o endereço do restaurante foram preenchidos
     if not data or not "name" in data or not "address" in data:    
         return jsonify({'message': 'O nome e o endereço do restaurante são obrigatórios'}), 400
     
+    #cria um novo endereço
     new_address_data = data['address']
     new_address = Address(
                         streetAddress = new_address_data['streetAddress'], 
@@ -27,6 +31,7 @@ def add_restaurant():
                         addressCountry = new_address_data['addressCountry']
                         )
 
+    #cria um novo restaurante
     new_restaurant = Restaurant(
                                 name=data['name'], 
                                 address=new_address, 
@@ -36,29 +41,37 @@ def add_restaurant():
                                 priceRange=data.get('priceRange')
                                 )
     
+    #adiciona o novo restaurante ao banco de dados
     db.session.add(new_restaurant)
     db.session.commit()
 
     return jsonify({'message': 'Restaurante inserido com sucesso'}), 201
 
+#Endpoint de listagem de restaurantes
 @app.route('/restaurants', methods=['GET'])
 def get_restaurants():
+    #retorna todos os restaurantes
     restaurantes = Restaurant.query.all()
     return jsonify([restaurante.serialize() for restaurante in restaurantes])
 
+#Endpoint de listagem de restaurante passando o ID
 @app.route('/restaurant/<restaurant_id>', methods=['GET'])
 def get_restaurant(restaurant_id):
+    #retorna o restaurante passando o ID, caso contrario dá 404
     restaurant = Restaurant.query.get_or_404(restaurant_id)
     return jsonify(restaurant.serialize())
 
+#Endpoint de listagem de restaurantes filtrando pelo endereço
 @app.route('/restaurants/address', methods=['GET'])
 def get_restaurantx():
     
+    #filtros
     address_country_filter = Address.addressCountry == request.args.get('addressCountry') if request.args.get('addressCountry') else True
     address_locality_filter = Address.addressLocality == request.args.get('addressLocality') if request.args.get('addressLocality') else True
     address_region_filter = Address.addressRegion == request.args.get('addressRegion') if request.args.get('addressRegion') else True
     address_street_filter = Address.streetAddress == request.args.get('streetAddress') if request.args.get('streetAddress') else True
     
+    #consulta
     restaurants = Restaurant.query.join(Address).filter(
         address_country_filter,
         address_locality_filter,
@@ -66,31 +79,36 @@ def get_restaurantx():
         address_street_filter
         ).all()
     
+    #serialização para o retorno
     serialized_restaurants = [restaurant.serialize() for restaurant in restaurants]   
     return jsonify({'restaurants': serialized_restaurants}), 200
 
+#Endpoint de atualização de restaurantes
 @app.route('/restaurant/<int:restaurant_id>', methods=['PUT'])
 def update_restaurant(restaurant_id):
     
+    #verifica se o restaurante existe
     restaurant = Restaurant.query.get(restaurant_id)
-
     if not restaurant:
         return jsonify({'message': 'Restaurante não existe'}), 404
 
+    #pega os dados do corpo da requisição
     data = request.json
-
     address_data = data.get('address')
 
+    #pega os dados do endereço
     if restaurant.address:
         address = restaurant.address
     else:
         address = Address()
 
+    #atualiza os dados do endereço
     address.streetAddress = address_data.get('streetAddress', address.streetAddress)
     address.addressLocality = address_data.get('addressLocality', address.addressLocality)
     address.addressRegion = address_data.get('addressRegion', address.addressRegion)
     address.addressCountry = address_data.get('addressCountry', address.addressCountry)
 
+    #atualiza os dados do restaurante
     restaurant.name = data.get('name', restaurant.name)
     restaurant.address = address
     restaurant.url = data.get('url', restaurant.url)
@@ -98,19 +116,21 @@ def update_restaurant(restaurant_id):
     restaurant.telephone = data.get('telephone', restaurant.telephone)
     restaurant.price_range = data.get('priceRange', restaurant.priceRange)
 
+    #atualiza o restaurante no banco de dados
     db.session.commit()
 
     return jsonify({'message': 'Restaurante atualizado com sucesso'}), 200
 
-
+#Endpoint de remoção de restaurantes
 @app.route('/restaurant/<int:id>', methods=['DELETE'])
 def delete_restaurant(id):
    
-    restaurant = Restaurant.query.get(id)
-   
+    #verifica se o restaurante existe
+    restaurant = Restaurant.query.get(id)   
     if not restaurant:
         return jsonify({'message': 'Restaurante não existe'}), 404
 
+    #deleta o restaurante
     db.session.delete(restaurant)
     db.session.commit()
     
